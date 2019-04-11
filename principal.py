5edb36a782d366fd2fca81c48a8daa6d5a5f6b33
Yuri Matheus <yurimatheusb@hotmail.com>
2019-04-11 19:16:28 -0300

    retorna a validacao do namespace __main__

--- principal.py
+++ principal.py
@@ -1,7 +1,9 @@
 import gerenciador_de_gastos
 import menu_ui
 
-arquivo = input('Digite o nome do arquivo que deseja abrir: ')
-gastos = gerenciador_de_gastos.abre_arquivo(arquivo)
 
-menu_ui.mostra_menu(gastos)
+if __name__ == '__main__':
+    arquivo = input('Digite o nome do arquivo que deseja abrir: ')
+    gastos = gerenciador_de_gastos.abre_arquivo(arquivo)
+
+    menu_ui.mostra_menu(gastos)
